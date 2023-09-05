06096d7f6c6d27f9a408d8d3ee8218d1ded4e1aa
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-06 01:30:36 +0300

    feat: add base WeekChart widget

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,23 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Spending during the week.'),
       ),
-      body: const Center(
-        child: Text('body'),
+      body: Column(
+        children: [
+          Card(
+              elevation: 5,
+              color: Colors.deepPurpleAccent,
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                width: double.infinity,
+                child: const Text('week chart'),
+              )),
+          // ),
+          const Card(
+            child: Text('list of transactions'),
+          )
+        ],
       ),
     );
   }
