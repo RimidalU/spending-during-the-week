b7d9e894b1cf5b9aceaef4f40404861e1b20cb0f
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-06 23:49:34 +0300

    refactor: change display code

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -2,9 +2,21 @@ import '/transaction.dart';
 
 final List<Transaction> transactions = [
   Transaction(
-      id: '1', title: 'New transaction', amount: 22.58, date: DateTime.now()),
+    id: '1',
+    title: 'New transaction',
+    amount: 22.58,
+    date: DateTime.now(),
+  ),
   Transaction(
-      id: '2', title: 'Mobile phone', amount: 10.5, date: DateTime.now()),
+    id: '2',
+    title: 'Mobile phone',
+    amount: 10.5,
+    date: DateTime.now(),
+  ),
   Transaction(
-      id: '3', title: 'Key repairer', amount: 5.3, date: DateTime.now()),
+    id: '3',
+    title: 'Key repairer',
+    amount: 5.3,
+    date: DateTime.now(),
+  ),
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import 'transaction.dart';
-
 import 'assets/constants.dart' as constants;
 
 void main() {
@@ -20,13 +18,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -40,44 +38,52 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         children: [
           Card(
-              elevation: 5,
-              color: Colors.deepPurpleAccent,
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                width: double.infinity,
-                child: const Text('week chart'),
-              )),
-          // ),
+            elevation: 5,
+            color: Colors.deepPurpleAccent,
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              width: double.infinity,
+              child: const Text('week chart'),
+            ),
+          ),
           Column(
             children: [
               ...constants.transactions.map((transaction) {
                 return Card(
-                    child: Row(children: [
-                  Container(
+                  child: Row(children: [
+                    Container(
                       padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 18),
+                        vertical: 10,
+                        horizontal: 18,
+                      ),
                       child: Text(
                         '\$${transaction.amount}',
                         style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.deepPurple),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
                           style: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w500),
-                          transaction.title),
-                      Text(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          transaction.title,
+                        ),
+                        Text(
                           style: const TextStyle(color: Colors.grey),
-                          DateFormat.yMMMMEEEEd().format(transaction.date))
-                    ],
-                  )
-                ]));
+                          DateFormat.yMMMMEEEEd().format(transaction.date),
+                        )
+                      ],
+                    )
+                  ]),
+                );
               })
             ],
           )
