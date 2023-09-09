fcfa18151422c7aaa253569ca8dd63175801b545
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-09 22:46:34 +0300

    feat: add  no content image

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -13,16 +13,23 @@ class TransactionsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 450,
-      child: ListView.builder(
-        itemBuilder: (_, index) {
-          return TransactionCard(
-            transactionAmount: transactions[index].amount,
-            transactionDate: transactions[index].date,
-            transactionTitle: transactions[index].title,
-          );
-        },
-        itemCount: transactions.length,
-      ),
+      child: transactions.isEmpty
+          ? Center(
+              child: Image.asset(
+                'assets/images/no-content.png',
+                fit: BoxFit.cover,
+              ),
+            )
+          : ListView.builder(
+              itemBuilder: (_, index) {
+                return TransactionCard(
+                  transactionAmount: transactions[index].amount,
+                  transactionDate: transactions[index].date,
+                  transactionTitle: transactions[index].title,
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
