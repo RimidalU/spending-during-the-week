28eac497bcc90c9ece53aa976fe91e7b38bd6435
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-10 20:41:02 +0300

    feat: implement conter amount transition for chart

--- lib/widgets/transactions_chart.dart
+++ lib/widgets/transactions_chart.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import './../models/transaction.dart';
+
 const dayInWeek = 7;
 
 class TransactionsChart extends StatelessWidget {
-  const TransactionsChart({super.key});
+  final List<Transaction> recentTransactions;
+
+  const TransactionsChart({super.key, required this.recentTransactions});
 
   List<Map<String, Object>> get groupedInDayTransactionsValue {
     return List.generate(dayInWeek, (index) {
       final weekDay = DateTime.now().subtract(
         Duration(days: index),
       );
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
       return {
-        'day': DateFormat.E(weekDay),
-        'amount': 9.99,
+        'day': DateFormat.E().format(weekDay),
+        'amount': totalSum,
       };
     });
   }
