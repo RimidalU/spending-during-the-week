71bf221faf7a3fbfc93a5883b4b914dfb15da760
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-11 00:54:51 +0300

    refactor: change days of transactions

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -11,12 +11,12 @@ final List<Transaction> transactions = [
     id: '2',
     title: 'Mobile phone',
     amount: 10.5,
-    date: DateTime.now(),
+    date: DateTime.now().subtract(const Duration(days: 1)),
   ),
   Transaction(
     id: '3',
     title: 'Key repairer',
     amount: 5.3,
-    date: DateTime.now(),
+    date: DateTime.now().subtract(const Duration(days: 2)),
   ),
 ];
