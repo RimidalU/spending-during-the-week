1938def2f982cbab02a2c23a404276666539524e
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-10 20:57:14 +0300

    feat: implement display transaction info per day

--- lib/widgets/transactions_chart.dart
+++ lib/widgets/transactions_chart.dart
@@ -38,10 +38,10 @@ class TransactionsChart extends StatelessWidget {
       elevation: 5,
       color: Colors.deepPurpleAccent,
       margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        width: double.infinity,
-        child: const Text('week chart'),
+      child: Row(
+        children: groupedInDayTransactionsValue.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
